91c0c51ea1fc9b869b5a7dbb2d94c42d58491e1b
Mika <makomitsuki@gmail.com>
2022-10-22 13:58:23 +0100

    status

--- src/components/Datatable/Datatable.jsx
+++ src/components/Datatable/Datatable.jsx
@@ -24,17 +24,17 @@ const columns = [
     {
       field: 'first_name',
       headerName: 'First Name',
-      width: 180,
+      width: 200,
     },
     {
         field: 'last_name',
         headerName: 'Last Name',
-        width: 180,
+        width: 200,
     },
     {
         field: 'address',
         headerName: 'Address',
-        width: 210,
+        width: 250,
     },
     {
         field: 'email',
@@ -44,9 +44,9 @@ const columns = [
     {
         field: 'order_status',
         headerName: 'Order Status',
-        width: 120,
+        width: 160,
         renderCell: (params) => {
-            return (<Chip label={params.row.order_status} size="small" />)
+            return (<span className={`orderStatus ${params.row.order_status}`}>{params.row.order_status}</span>)
         }
     },
   ];
@@ -55,8 +55,8 @@ const rows = [
     { id: 1, order_placed: '2021-10-05', product_name: 'cat keychain', price: 68.83, first_name: 'Jon', last_name: 'Snow', address: '123 North Rd', email: 'jonsnow@north.com', order_status: 'in_progress' },
     { id: 2, order_placed: '2021-08-27', product_name: 'i heart cat brooch', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'shipped' },
     { id: 3, order_placed: '2021-08-27', product_name: 'i heart cat brooch', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'shipped' },
-    { id: 4, order_placed: '2021-08-27', product_name: 'little canine', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'shipped' },
-    { id: 5, order_placed: '2021-11-12', product_name: 'i heart cat brooch', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'shipped' },
+    { id: 4, order_placed: '2021-08-10', product_name: 'little canine', price: 10.20, first_name: 'Sansa', last_name: 'Stark', address: '3123 South Rd', email: 'sansa@stark.uk', order_status: 'placed' },
+    { id: 5, order_placed: '2021-11-12', product_name: 'i heart cat brooch', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'cancelled' },
     { id: 6, order_placed: '2021-08-27', product_name: 'i heart cat brooch', price: 28.00, first_name: 'Daenerys', last_name: 'Targaryen', address: '123 North Rd', email: 'daenerys@dothraki.com', order_status: 'shipped' },
   ];
 
