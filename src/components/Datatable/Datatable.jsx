697b3358424ba7b6aca9ece84370a95fbc1f78b9
Mika <makomitsuki@gmail.com>
2022-10-22 13:20:01 +0100

    working datatable

--- src/components/Datatable/Datatable.jsx
+++ src/components/Datatable/Datatable.jsx
@@ -1,57 +1,49 @@
 import React from 'react';
 import "./Datatable.scss";
 import { DataGrid } from '@mui/x-data-grid';
-import Chip from '@mui/material/Chip';
 
 const columns = [
-    { field: 'order_id', headerName: 'Order ID', width: 70 },
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
     {
-        field: 'order_placed',
-        headerName: 'Order Placed',
-        type: "number"
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
     },
     {
-        field: 'product_name',
-        headerName: 'Product Name',
-      },
-    {
-        field: 'price',
-        headerName: 'Price',
-        type: "number"
-    },
-    {
-      field: 'first_name',
-      headerName: 'First Name',
-    },
-    {
-        field: 'last_name',
-        headerName: 'Last Name',
-    },
-    {
-        field: 'email',
-        headerName: 'Email',
-    },
-    {
-        field: 'order_status',
-        headerName: 'Order Status',
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
     },
   ];
-
-const rows = [
-    { order_id: 1, order_placed: '2021-10-05', product_name: 'cat keychain', price: 68.83, firstName: 'Jon', last_name: 'Snow', email: 'jonsnow@north.com', order_status: 'in_progress' },
-    { order_id: 2, order_placed: '2021-08-27', product_name: 'i heart cat brooch', price: 28.00, firstName: 'Daenerys', last_name: 'Targaryen', email: 'daenerys@dothraki.com', order_status: 'shipped' },
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   ];
 
 const Datatable = () => {
     return (
         <div className='datatable'>
-            {/*<DataGrid
+            <DataGrid
                 rows={rows}
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
-    />*/}
-            hi
+            />
         </div>
     )
 }
